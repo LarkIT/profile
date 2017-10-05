f793b6659d25bf0d4578701cdedd45f51613442c
Nicolas Fosdick <nick.fosdick@sigfig.com>
2017-10-04 22:57:42 -0600

    minor

--- manifests/gitlab.pp
+++ manifests/gitlab.pp
@@ -23,8 +23,8 @@ class profile::gitlab (
 #  ssl_certificate_key: /etc/puppetlabs/puppet/ssl/private_keys/%{trusted.certname}.pem
 
 #  include ::gitlab
-  class{ '::gitlab':
-#    external_url => https://${::fqdn},
+  class{ 'gitlab':
+    external_url => "https://${::fqdn}",
 #    nginx => { 
 #               ssl_certificate     => "/etc/puppetlabs/puppet/ssl/certs/%{trusted.certname}.pem",
 #               ssl_certificate_key => /etc/puppetlabs/puppet/ssl/private_keys/%{trusted.certname}.pem,

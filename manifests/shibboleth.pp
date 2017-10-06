eb26060f66b081e5d4546109dd38d5b0a6969668
Nicolas Fosdick <nick.fosdick@sigfig.com>
2017-10-06 10:00:46 -0600

    mionr

--- manifests/shibboleth.pp
+++ manifests/shibboleth.pp
@@ -19,31 +19,31 @@ class profile::shibboleth (
   $generate_cert = true,
 ) {
   #Include repo
-  include ::repos::shibboleth
+#  include ::repos::shibboleth
 
-  ensure_packages('wget')
+#  ensure_packages('wget')
 
   # Set up Apache Module
-  include ::apache::mod::shib
+#  include ::apache::mod::shib
 
   # Initialise Shibboleth configuration and services
-  include ::shibboleth
+#  include ::shibboleth
 
   # Set up the Shibboleth Single Sign On (sso) module
-  create_resources('::shibboleth::sso', $sso)
-  create_resources('::shibboleth::metadata', $metadata)
-  create_resources('::shibboleth::attribute_map', $attribute_map)
+#  create_resources('::shibboleth::sso', $sso)
+#  create_resources('::shibboleth::metadata', $metadata)
+#  create_resources('::shibboleth::attribute_map', $attribute_map)
 
-  if $generate_cert {
-    include shibboleth::backend_cert
-  }
+#  if $generate_cert {
+#    include shibboleth::backend_cert
+#  }
 
-  file{ '/etc/shibboleth/attribute-map.xml':
-    ensure => file,
-    owner  => 'shibd',
-    group  => 'shibd',
-    mode   => '0644',
-    source => "puppet:///modules/${module_name}/attribute-map.xml",
-    notify => Service[ 'shibd' ],
-  }
+#  file{ '/etc/shibboleth/attribute-map.xml':
+#    ensure => file,
+#    owner  => 'shibd',
+#    group  => 'shibd',
+#    mode   => '0644',
+#    source => "puppet:///modules/${module_name}/attribute-map.xml",
+#    notify => Service[ 'shibd' ],
+#  }
 }

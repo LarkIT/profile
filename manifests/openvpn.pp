c6171cccf9b4b3739e23e33756d2674476e9d635
Nicolas Fosdick <nick.fosdick@sigfig.com>
2017-11-01 09:15:36 -0600

    minor

--- manifests/openvpn.pp
+++ manifests/openvpn.pp
@@ -18,8 +18,7 @@ class profile::openvpn (
   include ::repos::openvpn
 
   # OpenVPN_AS - please see https://github.com/LarkIT/puppet-openvpn_as
-  #include ::openvpn_as
-  include ::openvpn
+  include ::openvpn_as
 
   # Firewall should be handled by OpenVPN-AS Software?
 

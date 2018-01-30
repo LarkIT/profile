76b12d5c70ac6c83311cc88ad45fef50c69cdb83
Andrew Alles <andrew@lark-it.com>
2018-01-30 13:32:27 -0700

    Removed pulp_client inclusion from railsapp (???)

--- manifests/railsapp.pp
+++ manifests/railsapp.pp
@@ -16,7 +16,7 @@ class profile::railsapp (
   Array $additional_packages = [],
   Array $additional_classes = [],
 ) {
-include profile::pulp_client
+  #include profile::pulp_client
   # Yes we know its bad
   $standard_railsapp_packages = [
   'nano',

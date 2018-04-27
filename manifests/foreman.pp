0133696e64d4eafbf4254b3daeb781b0ca65ec1a
Costa Arigirs <costa.argiris@lark-it.com>
2018-04-27 14:15:08 -0600

    Commented out puppetdb classes for foreman
    DEVOPS-651

--- manifests/foreman.pp
+++ manifests/foreman.pp
@@ -13,9 +13,9 @@ class profile::foreman (
   include r10k
   include r10k::webhook::config
   include puppetdb::master::config
-  class { 'puppetdb::server':
-    database_host => 'localhost',
-  }
+  #class { 'puppetdb::server':
+  #  database_host => 'localhost',
+  #}
   # From KAFO (forman installer) -
   #  (/usr/share/gems/gems/kafo-2.0.0/modules/kafo_configure/manifests/init.pp)
   include foreman

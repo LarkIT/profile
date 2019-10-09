3017168f4ba604a191a7741d413f460ec8fc9f76
Ethan Claassen <ethan.claassen@lark-it.com>
2019-10-09 14:35:58 -0600

    Added Zabbix Java gateway class

---
+++ manifests/zabbix/java_gateway.pp
@@ -0,0 +1,10 @@
+#
+# Class: profile::zabbix::javagateway
+# Purpose: Install and setup the Zabbix proxy for monitoring
+#
+#
+# Status: Work in Progress
+#
+class profile::zabbix::javagateway (){
+  include zabbix::javagateway
+}
